77f2e1f148c335eb460ff178cc0ae1ab164c9905
pulsar <2248908509@qq.com>
2023-05-06 16:12:02 +0800

    基础语法作业

--- ch1/homework.go
+++ ch1/homework.go
@@ -1,49 +1,53 @@
-/*
-设计一个游戏人物的结构体，包含人物的名字、等级、经验值、血量和攻击力等信息。然后设计一个接口，该接口有一个 `Attack` 方法，表示该角色攻击另一个角色。该方法接受一个 `Person` 类型的参数，表示被攻击的角色。实现该接口的结构体需要具备攻击能力，即能够将被攻击角色的血量减少相应的攻击力值。
-
-- 接口的 `Attack` 方法需要在被攻击角色的血量上减去攻击者的攻击力值；
-- 结构体的名字、等级、经验值、血量和攻击力等信息需要在创建时初始化。
-- 在 `main` 函数中创建两个角色，然后让其中一个角色攻击另一个角色，打印被攻击角色的血量变化情况。
-*/
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-type Person struct {
-	name            string
-	level           int
-	experienceValue int
-	bloodVolume     int
-	aggressivity    int
-}
-
-func (p Person) Attack(a Person) Person {
-	a.bloodVolume -= p.aggressivity
-	return a
-}
-
-type game interface {
-	Attack(a Person) Person
+func guessNumber(secretNumber int) int {
+	var yourNumber int
+	var result int
+	for {
+		fmt.Print("Please enter your guess: ")
+		_, err := fmt.Scanf("%d", &yourNumber)
+		fmt.Scanln() //读取缓冲区的回车
+		if err != nil {
+			fmt.Println("Invalid input, please try again.")
+			continue
+		}
+		if yourNumber == secretNumber {
+			result = 0
+			break
+		}
+		if yourNumber < secretNumber {
+			result = 1
+		}
+		if yourNumber > secretNumber {
+			result = 2
+		}
+		break
+	}
+	return result
 }
 
 func main() {
-	jack := Person{
-		name:            "jack",
-		level:           10,
-		experienceValue: 54,
-		bloodVolume:     100,
-		aggressivity:    5,
+	maxNum := 100
+	rand.Seed(time.Now().UnixNano())
+	secretNumber := rand.Intn(maxNum)
+	var count int
+	for {
+		receiveNumber := guessNumber(secretNumber)
+		count++
+		switch receiveNumber {
+		case 0:
+			fmt.Printf("You are right! It took you %d guesses.\n", count)
+			return
+		case 1:
+			fmt.Println("Guess it's small")
+		case 2:
+			fmt.Println("Guess it's big")
+		}
 	}
-	kite := Person{
-		name:            "kite",
-		level:           20,
-		experienceValue: 23,
-		bloodVolume:     200,
-		aggressivity:    10,
-	}
-	var a game
-	a = jack
-	fmt.Printf("jack攻击kite前kite的血量为%d\n", kite.bloodVolume)
-	kite = a.Attack(kite)
-	fmt.Printf("攻击后kite的血量为%d\n", kite.bloodVolume)
 }
